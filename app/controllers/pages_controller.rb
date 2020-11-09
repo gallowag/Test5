963ee4fa95058e897549076912e049ee0f615926
gallowag <gallowaygreer@gmail.com>
2020-11-09 16:20:54 -0700

    added more files and nav bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   def index
     @title = 'Welcome'
   end
+  def about
+    @title = 'About Us'
+    @content = 'Little blurb mom is going to write'
+  end
+
+  def opportunities
+    @title = 'Opportunities'
+    @content = 'Little blurb mom is going to write'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  get 'about' => 'pages#about', as: 'about'
+  get 'opportunities' => 'pages#opportunities', as: 'opportunities'
+  
   root 'pages#index', as: 'home'
 
 end
